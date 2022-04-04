99d2ca3b6e2993305d3dddf590822132a5e6de3c
Yotakey <keyisakris@gmail.com>
2022-04-04 11:09:17 +0700

    DataBase Configuration

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from core.config import settings
 from fastapi.staticfiles import StaticFiles
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base import Base
 
 
 def include_router(app):
@@ -10,12 +12,17 @@ def include_router(app):
 def configure_static(app):
     app.mount("/static", StaticFiles(directory = "static"), name="static")
 
+def create_tables():
+    print("create_tables")
+    Base.metadata.create_all(bind=engine)
+
 def start_application():
     app = FastAPI(title = settings.PROJECT_NAME,
                     version = settings.PROJECT_VERSION
     )
     include_router(app)
     configure_static(app)
+    create_tables()
     return app
 
 app = start_application()
